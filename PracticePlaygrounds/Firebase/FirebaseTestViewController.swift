66bc54a17645e1a5722c07d6c2839f367420e9df
Zhiyuan Zhou <zhou.zhiyuan.stephen@hotmail.com>
2022-11-05 03:12:25 -0400

    firebase

---
+++ PracticePlaygrounds/Firebase/FirebaseTestViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FirebaseTestViewController.swift
+//  PracticePlaygrounds
+//
+//  Created by Zhou Zhiyuan on 10/18/22.
+//
+
+import Foundation
+import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+
+class FirebaseTestViewController: UIViewController {
+  
+}
